d0f0e8ae7d07ca0d9ddadb6187ea484b5c3a8f32
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-26 13:40:21 +0100

    primer commit

--- public/javascripts/addPlaces.js
+++ public/javascripts/addPlaces.js
@@ -1,42 +1,81 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-
-  
-    const mapAddPlaces = new google.maps.Map(document.getElementById('mapAddPlaces'), {
-      zoom: 18,
-    });
-
-    geolocalize().then(center => {
-      mapAddPlaces.setCenter(center);
-    });
-
-    const setPosOnForm = (latlng) => {
-      document.getElementById('lat-pos').value = latlng.lat;
-      document.getElementById('lng-pos').value = latlng.lng;
-    }
-  
-    let marker;
-  
-    mapAddPlaces.addListener('click', function(e) {
-      const clickPos = {
-        lat:e.latLng.lat(),
-        lng:e.latLng.lng()
-      }
-      console.log(clickPos);
-      marker.setPosition(clickPos);
-      setPosOnForm(clickPos)
-    });
-  
-    geolocalize().then(center => {
-      mapAddPlaces.setCenter(center);
-      marker = new google.maps.Marker({
-        position: center,
-        map: mapAddPlaces
+    var mapAddPlaces, infoWindow;
+    function geolocalize() {
+      mapAddPlaces = new google.maps.Map(document.getElementById("mapAddPlaces"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 6
       });
-      setPosOnForm(center);
-    });
-  
+      infoWindow = new google.maps.InfoWindow();
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Location found.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+          },
+          function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+      infoWindow.open(map);
+    }
+
+    // const mapAddPlaces = new google.maps.Map(document.getElementById('mapAddPlaces'), {
+    //   zoom: 18,
+    // });
+
+    // geolocalize().then(center => {
+    //   mapAddPlaces.setCenter(center);
+    // });
+
+    // const setPosOnForm = (latlng) => {
+    //   document.getElementById('lat-pos').value = latlng.lat;
+    //   document.getElementById('lng-pos').value = latlng.lng;
+    // }
+
+    // let marker;
+
+    // mapAddPlaces.addListener('click', function(e) {
+    //   const clickPos = {
+    //     lat:e.latLng.lat(),
+    //     lng:e.latLng.lng()
+    //   }
+    //   console.log(clickPos);
+    //   marker.setPosition(clickPos);
+    //   setPosOnForm(clickPos)
+    // });
+
+    // geolocalize().then(center => {
+    //   mapAddPlaces.setCenter(center);
+    //   marker = new google.maps.Marker({
+    //     position: center,
+    //     map: mapAddPlaces
+    //   });
+    //   setPosOnForm(center);
+    // });
   },
   false
 );
