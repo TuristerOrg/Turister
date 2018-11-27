a26e12cfb7a61259b94998d18371b0cd39c595c9
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-27 17:20:47 +0100

    added axios

--- routes/index.js
+++ routes/index.js
@@ -1,77 +1,48 @@
-<<<<<<< HEAD
-
 const express = require('express');
 const passport = require('passport');
+const axios = require('axios');
 
-const router  = express.Router();
-const Place = require('../models/Place');
-
-router.get('/', (req, res, next) => {
-  Place.find({}).then((place) => {
-    res.render('index', { places : JSON.stringify(place) });
-  });
-=======
-const express = require("express");
-const passport = require("passport");
-const axios = require("axios");
 const router = express.Router();
-const Place = require("../models/Place");
+const Place = require('../models/Place');
 /* GET home page */
 
-router.get("/", (req, res, next) => {
-  
+router.get('/', (req, res, next) => {
   //   let petition = new Promise ((resolve, rej)=>{
   //   peticiones()
   //   resolve('success')
   // })
 
   // petition.then(response =>{
-    Place.find().then(response => {
-      console.log(response)
-        res.render("index", {response: JSON.stringify(response)})
-        })
-      .catch(errGetAPI => {
-        console.log(errGetAPI);
-      });
+  Place.find().then((response) => {
+    console.log(response);
+    res.render('index', { response: JSON.stringify(response) });
+  })
+    .catch((errGetAPI) => {
+      console.log(errGetAPI);
+    });
   // })
->>>>>>> e5c39dd7b70cdc5afec5e9602266575e43f2f3fd
 });
 
 router.get('/addplace', (req, res, next) => {
   res.render('add/place');
 });
 
-<<<<<<< HEAD
 router.post('/addplace', (req, res, next) => {
   const currentPlace = {
-    name: req.body.name,
-    description: req.body.description,
+    name: req.body.placename,
+    description: req.body.placedescription,
+    type: req.body.placetype,
     location: {
       type: 'Point',
       coordinates: [Number(req.body.latitude), Number(req.body.longitude)],
     },
   };
-  Place.create(currentPlace).then((currentPlace) => {
-    res.redirect('/');
-  }).catch(e => next(e));
-=======
-router.post("/addplace", (req, res, next) => {
-  let currentPlace = {
-    name: req.body.placename,
-    description: req.body.placedescription,
-    type: req.body.placetype,
-    location: {
-      type: "Point",
-      coordinates: [Number(req.body.latitude), Number(req.body.longitude)]
-    }
-  };
   // console.log(currentPlace);
   Place.create(currentPlace)
-    .then(currentPlace => {
-      res.redirect("/");
+    .then((currentPlace) => {
+      res.redirect('/');
     })
     .catch(e => next(e));
->>>>>>> e5c39dd7b70cdc5afec5e9602266575e43f2f3fd
 });
 
 // let petitionAJAX = (url, tipo) => {
