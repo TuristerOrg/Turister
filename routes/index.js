f2a38bc5e2586ed156a816af9ff0395d5f7efab8
Ricardo Cefalo <ricardo.cefalo@gmail.com>
2018-11-27 20:47:49 +0100

    Added admin panel

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
 const express = require('express');
 const passport = require('passport');
 const axios = require('axios');
 
 const router = express.Router();
 const Place = require('../models/Place');
-/* GET home page */
 
 router.get('/', (req, res, next) => {
   //   let petition = new Promise ((resolve, rej)=>{
@@ -44,53 +47,5 @@ router.post('/addplace', (req, res, next) => {
     })
     .catch(e => next(e));
 });
-
-// let petitionAJAX = (url, tipo) => {
-//   axios
-//     .get(url)
-//     .then(response => {
-//       response.data["@graph"].forEach(element => {
-//         if(element.location!=undefined){
-//         let currentMonument = {
-//           name: element.title,
-//           description: element.organization["organization-desc"],
-//           type: tipo,
-//           location: {
-//             type: "Point",
-//             coordinates: [
-//               Number(element.location.latitude),
-//               Number(element.location.longitude)
-//             ]
-//           }
-//         };
-//         Place.create(currentMonument).catch(e =>
-//           console.log(`Error in element${element.name}`)
-//         );
-//         }else{
-//           return
-//         }
-//       });
-//     })
-//     .catch(errGetAPI => {
-//       console.log(errGetAPI);
-//     });
-// };
-
-// let peticiones = ()=>{
-// Place.collection.drop()
-
-//   petitionAJAX(
-//     "https://datos.madrid.es/egob/catalogo/201105-0-informacion-turismo.json","informacion"
-//   );
-//   petitionAJAX(
-//     "https://datos.madrid.es/egob/catalogo/208844-0-monumentos-edificios.json","monumento"
-//   );
-//   petitionAJAX(
-//     "https://datos.madrid.es/egob/catalogo/209426-0-templos-catolicas.json","iglesia"
-//   );
-//   petitionAJAX(
-//     "https://datos.madrid.es/egob/catalogo/201132-0-museos.json","museo"
-//   );
-// }
 
 module.exports = router;
