97de9d80b75b20c3a4c4408840dc30606604efd9
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-27 20:47:59 +0100

    filtros de mapa ok

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -7,4 +7,12 @@ window.onload = function() {
       menu.style.display = "block";
     }
   };
+  document.getElementById("filter-btn").onclick = function() {
+    var menu = document.getElementById("filter-container");
+    if (menu.style.display === "flex") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
+  };
 };
