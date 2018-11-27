7b736715aedca3a891e9ba6019e8ded18d208f1d
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-28 00:47:16 +0100

    Infowindos parece en todos los puntos con mismo valor

--- public/javascripts/navMap.js
+++ public/javascripts/navMap.js
@@ -42,14 +42,46 @@ document.addEventListener(
 
       const printInfoMarkers = () => {
         info.forEach((item) => {
-          console.log(item);
+          // console.log(item.name);
+
+          var infowindow = new google.maps.InfoWindow();
+
           marker = new google.maps.Marker({
             position: {
               lat: item.location.coordinates[0],
               lng: item.location.coordinates[1],
             },
-            map,
+            map: map,
+            title: item.name
           });
+
+          google.maps.event.addListener(marker,'click', (function(marker) {
+            return function(){
+              infowindow.setContent('<div id="content">'+
+              '<div id="siteNotice">'+
+              '</div>'+
+              '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+              '<div id="bodyContent">'+
+              '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+              'sandstone rock formation in the southern part of the '+
+              'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+              'south west of the nearest large town, Alice Springs; 450&#160;km '+
+              '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+              'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+              'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+              'Aboriginal people of the area. It has many springs, waterholes, '+
+              'rock caves and ancient paintings. Uluru is listed as a World '+
+              'Heritage Site.</p>'+
+              '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+              'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+              '(last visited June 22, 2009).</p>'+
+              '</div>'+
+              '</div>');
+    
+              infowindow.open(map, marker);
+            }
+          })(marker));
+
         });
       };
 
